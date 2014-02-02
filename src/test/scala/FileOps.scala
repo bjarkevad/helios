c745c87051a66bff44f7d55e8125e4a53826c2f4
Bjarke Vad <bjarke.vad90@gmail.com>
2014-02-02 17:58:44 +0100

    FileOps test: creating file with attributes

--- src/test/scala/FileOps.scala
+++ src/test/scala/FileOps.scala
@@ -2,13 +2,15 @@ package helios.util.nio.test
 
 import org.scalatest._
 
-import java.nio.file.{Paths, Files}
+import java.nio.file.{LinkOption, Path, Paths, Files}
 
 import helios.util.nio.FileOps._
+import java.nio.file.attribute.{PosixFilePermissions, PosixFileAttributes}
 
 class FileOps extends FlatSpec with Matchers {
   "FileOps" should "be able to create a file without attributes" in {
     val file = "./testfile1"
+    Files.deleteIfExists(Paths.get(file))
 
     createFile(file).recover {
       case f: Throwable => {
@@ -16,6 +18,29 @@ class FileOps extends FlatSpec with Matchers {
         "Nope"
       }
     }.get.toString should be(file)
+    Files.deleteIfExists(Paths.get(file)) should be(true)
+  }
+
+  "FileOps" should "be able to create a file with attributes" in {
+    import collection.JavaConverters._
+
+    val file = "./testfile1"
+    Files.deleteIfExists(Paths.get(file))
+
+    val path: Path = createFile(file, PosixFilePermissions.asFileAttribute(PosixFilePermissions.fromString("rwx------"))).recover {
+      case f: Throwable => {
+        println(f)
+        Paths.get("Nope")
+      }
+    }.get
+
+    path.toString should be(file)
+
+    val expected = Seq("OWNER_READ", "OWNER_EXECUTE", "OWNER_WRITE")
+    val actual = Files.readAttributes(path, "posix:permissions", LinkOption.NOFOLLOW_LINKS).
+      asScala("permissions").toString
+    expected should be (expected.filter(actual.contains))
+
     Files.deleteIfExists(Paths.get(file)) should be(true)
   }
 
@@ -33,7 +58,7 @@ class FileOps extends FlatSpec with Matchers {
 
   it should "be able to delete a file if it exists" in {
     val file = "./testfile1"
-    createFile(file)
+    Files.createFile(Paths.get(file))
     val res = deleteIfExists(file)
     res.isSuccess should be(true)
     res.get should be (true)
