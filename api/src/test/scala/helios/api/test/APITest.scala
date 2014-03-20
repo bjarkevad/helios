9b86797a9a9253ee11a5e6b3737398940c39459b
Bjarke Vad <bjarke.vad90@gmail.com>
2014-03-20 21:11:39 +0100

    Empty API test file

---
+++ api/src/test/scala/helios/api/test/APITest.scala
@@ -0,0 +1,28 @@
+package helios.api.test
+
+import org.scalatest._
+import akka.testkit.{ImplicitSender, TestKit}
+import akka.actor.ActorSystem
+
+class APITest extends TestKit(ActorSystem("APITest"))
+with ImplicitSender
+with FlatSpecLike
+with Matchers
+with BeforeAndAfter {
+
+  before {
+
+  }
+
+  after {
+
+  }
+
+  "APITest" should "" in {
+
+  }
+
+  it should "" in {
+
+  }
+}
