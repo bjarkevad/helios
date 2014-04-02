e8bd77487b66566c176443463820156a229b247c
Bjarke Vad <bjarke.vad90@gmail.com>
2014-04-02 06:52:11 +0200

    unbuildable stuff

---
+++ src/main/scala/helios/core/actors/flightcontroller/GenericUART.scala
@@ -0,0 +1,15 @@
+package helios.core.actors.flightcontroller
+
+import akka.actor.Actor
+
+object GenericUART {
+
+}
+
+class GenericUART(uartManager) extends Actor {
+
+  override def receive: Receive = {
+    case _ =>
+
+  }
+}
